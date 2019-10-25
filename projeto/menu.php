f4a8897ff7a5aafc065fe2c18a984b0228fa56ff
joaocarloslima <joaocarloslima@me.com>
2019-10-24 22:13:16 -0300

    ponto de entrada e navegação

--- projeto/menu.php
+++ projeto/menu.php
@@ -1,18 +1,20 @@
-	<div class="container-fluid">	
-		<div class="row">
-			<div class="col-2">
-				<ul class="nav flex-column nav-pills vertical">
-					<li class="nav-item">
-						<a class="nav-link active" href="#">Usuários</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Questões</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Alternativas</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Ranking</a>
-					</li>
-				</ul>
-			</div>
+<?php $recurso = $_SERVER["PATH_INFO"] ?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-2">
+			<ul class="nav flex-column nav-pills vertical">
+				<li class="nav-item">
+					<a class="nav-link <?= ($recurso=='/usuarios')?'active':'' ?>" href="/usuarios">Usuários</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link <?= ($recurso=='/questoes')?'active':'' ?>" href="/questoes">Questões</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link <?= ($recurso=='/alternativas')?'active':'' ?>" href="/alternativas">Alternativas</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link <?= ($recurso=='/ranking')?'active':'' ?>" href="/ranking">Ranking</a>
+				</li>
+			</ul>
+		</div>
