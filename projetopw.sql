bcba64c563d5db8eed0df0cf761ec2ba2bf2fd37
João Carlos Lima e Silva <joaocarloslima@me.com>
2019-11-07 15:16:27 -0300

    atualizacao do BD

--- projetopw.sql
+++ projetopw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 24-Out-2019 às 16:45
+-- Generation Time: 07-Nov-2019 às 19:12
 -- Versão do servidor: 10.1.37-MariaDB
 -- versão do PHP: 7.2.12
 
@@ -25,6 +25,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `alternativas`
+--
+
+CREATE TABLE `alternativas` (
+  `idAlternativa` int(11) NOT NULL,
+  `idQuestao` int(11) NOT NULL,
+  `texto` varchar(200) NOT NULL,
+  `correta` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `alternativas`
+--
+
+INSERT INTO `alternativas` (`idAlternativa`, `idQuestao`, `texto`, `correta`) VALUES
+(5, 4, '4', 1),
+(6, 4, '22', 0),
+(7, 3, 'Cabral', 1),
+(8, 3, 'Bacal', 0),
+(9, 3, 'Colombo', 0),
+(13, 4, '33', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `questoes`
 --
 
@@ -39,7 +64,8 @@ CREATE TABLE `questoes` (
 --
 
 INSERT INTO `questoes` (`idQuestao`, `enunciado`, `tipo`) VALUES
-(1, 'Quem descobriu o Brazil?', 'alternativas');
+(3, 'Quem descobriu o Braza?', 'alternativas'),
+(4, 'Quanto Ã© 2+2 ?', 'numÃ©rica');
 
 -- --------------------------------------------------------
 
@@ -61,11 +87,18 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`idUsuario`, `nome`, `email`, `senha`) VALUES
 (6, 'Carlos Roberto', 'carlosalberto@gmail.com.br', 'etec238'),
 (7, 'Joao Carlos Lima', 'joao@etecia.com.br', '456'),
-(8, 'Carla Ferreira', 'carla@pedreira.com', '83b4ef5ae4bb360c96628aecda974200');
+(8, 'JoÃ£o Carlos Lima e Silva', 'joao@gmail.com', 'e10adc3949ba59abbe56e057f20f883e');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `alternativas`
+--
+ALTER TABLE `alternativas`
+  ADD PRIMARY KEY (`idAlternativa`),
+  ADD KEY `alternativa_questao` (`idQuestao`);
 
 --
 -- Indexes for table `questoes`
@@ -84,84 +117,32 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT for table `alternativas`
+--
+ALTER TABLE `alternativas`
+  MODIFY `idAlternativa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `questoes`
 --
 ALTER TABLE `questoes`
-  MODIFY `idQuestao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idQuestao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- phpMyAdmin SQL Dump
--- version 4.8.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 12-Set-2019 às 03:13
--- Versão do servidor: 10.1.33-MariaDB
--- PHP Version: 7.2.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `projetopw`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `idUsuario` int(11) NOT NULL,
-  `nome` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `senha` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`idUsuario`, `nome`, `email`, `senha`) VALUES
-(1, 'joao', 'joao@etecia.com', '123');
-
---
--- Indexes for dumped tables
+-- Constraints for dumped tables
 --
 
 --
--- Indexes for table `usuarios`
+-- Limitadores para a tabela `alternativas`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`idUsuario`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `alternativas`
+  ADD CONSTRAINT `alternativa_questao` FOREIGN KEY (`idQuestao`) REFERENCES `questoes` (`idQuestao`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
